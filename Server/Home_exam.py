0d27d4c0ffdf94cf043459937b34d9428ef6ba26
Yuvalkeidar <Yuvalkeidar94@gmail.com>
2018-02-08 23:15:52 +0200

    commit

--- Server/Home_exam.py
+++ Server/Home_exam.py
@@ -2,17 +2,16 @@ import sys
 # import the flask_cors from package add that path
 sys.path.insert(0, './packages/')
 
-from threading import Thread
 # service - webservice declartion and global list of tasks
-from service   import tasks,app,socketio
+from service   import tasks,app,socketio,updateTasks
 # the worker print to log and delete the first task in the sorted list
+
 from worker    import executeTask
 
 if __name__ == '__main__':
 
 # # separte thread for running in background when the service is alive
-    worker = Thread(target=executeTask,args=[socketio])
-    worker.start()
+    executeTask()
 
 # open port for webservice connection
     socketio.run(app,port=5002)
--- Server/worker.py
+++ Server/worker.py
@@ -1,16 +1,14 @@
 import sched, time,datetime
 from dataFile import tasks,updateDataFile
+import threading
 
-# this function running when the webserive availeble
-# and "execute" the first task, and delete from the global list of tasks
-def executeTask(socketio):
-    while True:
-        time.sleep(60)
-        if len(tasks) > 0:
-            print "task executed {} at {} with priorty {} and execute time {}".format( \
-                tasks[0]['payload'], \
-                datetime.datetime.now(), \
-                tasks[0]['priorty'],tasks[0]['eTime'] \
-            )
-            del tasks[0]
-            updateDataFile(tasks)
+def executeTask():   
+    threading.Timer(60.0,executeTask).start()
+    if len(tasks) > 0:
+        print "task executed {} at {} with priorty {} and execute time {}".format( \
+            tasks[0]['payload'], \
+            datetime.datetime.now(), \
+            tasks[0]['priorty'],tasks[0]['eTime'] \
+        )
+        del tasks[0]
+        updateDataFile(tasks)
